109d0b74eb2f60b134d1207b9d70c39ef1a29815
shashank <shashank.sj1@programmer.net>
2018-01-30 00:21:55 -0500

    Add files via upload

---
+++ mkfile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Enter the file name?"
+read FILE_NAME
+echo "I will create you a file called ${FILE_NAME}.sh"
+touch "${FILE_NAME}.sh"
+echo "Please type commands and end with  ctrl+d"
+cat >> "${FILE_NAME}.sh"
+echo "all done ur file is created"
